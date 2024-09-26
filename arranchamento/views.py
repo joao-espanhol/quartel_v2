c43b69b25ac64a9929fbde64af41d58a8df39268
João Espanhol <jpespanhol300@gmail.com>
2024-09-26 09:20:41 -0300

    Teste 6

--- arranchamento/views.py
+++ arranchamento/views.py
@@ -102,7 +102,7 @@ def excluir_arranchamento(request, arranchamento_id):
 def verificar_arranchamentos(request):
     if request.method == 'GET':
         hoje = timezone.now().date()
-        primeiro_dia = hoje + timedelta(days=3)
+        primeiro_dia = hoje + timedelta(days=5)
         
         ordem_postos = {
             'Soldado': 1,
